d9d7779d7b2c7bd53d2608393e5b8b59b0e1996d
exanimodsg-beep <exanimodsg@gmail.com>
2026-03-04 10:21:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/app', express.static(path.join(__dirname, 'app')));
 
 const PORT = process.env.PORT || 3000;
 
